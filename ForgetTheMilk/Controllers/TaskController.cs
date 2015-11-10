15b9bbd46062f1efa999a4dcf2834d983befa731
Anna Eklund <vitastjern@gmail.com>
2015-11-10 10:34:09 +0100

    init commit

---
+++ ForgetTheMilk/Controllers/TaskController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ForgetTheMilk.Controllers
+{
+    public class TaskController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(Tasks);
+        }
+
+        public static readonly List<Task> Tasks = new List<Task>();
+
+        [HttpPost]
+        public ActionResult Add(string task)
+        {
+            var taskItem = new Task { Description = task };
+            var dueDatePattern = new Regex(@"may\s(\d)");
+            var hasDueDate = dueDatePattern.IsMatch(task);
+            if (hasDueDate)
+            {
+                var dueDate = dueDatePattern.Match(task);
+                var day = Convert.ToInt32(dueDate.Groups[1].Value);
+                taskItem.DueDate = new DateTime(DateTime.Today.Year, 5, day);
+            }
+            Tasks.Add(taskItem);
+            return RedirectToAction("Index");
+        }
+    }
+
+    public class Task
+    {
+        public string Description { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
